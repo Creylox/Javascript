d87ab0d77a25ac794c0b83c9fdaef252c521787c
Estéfano <102473620+Creylox@users.noreply.github.com>
2023-01-26 11:19:23 -0300

    Add files via upload

---
+++ 02 - Lista.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Estefano",
+    24,
+    false,
+    new Date (1998, 07, 20),
+    {
+        titulo: "1984",
+        autor: "George Orwell",
+        fecha: new Date (1949, 06, 08),
+        url: "https://www.amazon.com/-/es/George-Orwell/dp/B08T49SJZT/ref=sr_1_2?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=15P3V306UBIIR&keywords=1984&qid=1674742375&sprefix=198%2Caps%2C240&sr=8-2"
+    }
+]
